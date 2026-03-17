7d2d3f439df33f7d4d79564a324370970b1bd0d4
luquetabertolini <lubertolini1104@gmail.com>
2026-03-16 22:42:46 -0300

    aula 02 de python

---
+++ aulas02-variaveis-operadores/aulas02a-variaveis.py
@@ -0,0 +1,27 @@
+print("olá mundo")
+
+print(7 + 4)
+print("7" + "4")
+
+'''
+Comentários de 
+múltiplas linhas
+'''
+
+#VARIÁVEIS
+
+nome = "ALEXANDRE" #str
+idade = 26 # int
+peso = 70.2 #float
+
+print(nome, idade, peso)
+
+#INPUT - SIMULAR UM FORMULÁRIO
+nome = input("Digite seu nome: ") #string
+print(nome)
+idade = int(input("Digite sua idade: "))
+peso= float(input("Digite seu peso: "))
+
+print(nome, idade, peso)
+
+
